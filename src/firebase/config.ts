5033d8ac365345eaf49000d01e6fa274d39df0b8
Worku Mesafint <workumesafint9@gmail.com>
2026-02-20 14:03:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2513460272-12225",
+  "appId": "1:374848476323:web:e6995ee5b1b16a6b9b1b16",
+  "apiKey": "AIzaSyAPgXiN91zLNVpAh_p46hEDxAWhNhSsQnY",
+  "authDomain": "studio-2513460272-12225.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "374848476323"
+};
