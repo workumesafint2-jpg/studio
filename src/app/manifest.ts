83543b5c2c5d762a9619118e769ab22d081f4071
Worku Mesafint <workumesafint9@gmail.com>
2026-02-27 14:39:43 +0000

    "I have pushed the latest fixes from the other computer. Please confirm

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,6 @@
 
 /**
- * @fileOverview Manifest.ts is deprecated in v3.0.0 in favor of public/manifest.json
+ * @fileOverview Manifest.ts is deprecated in v3.0.1 in favor of public/manifest.json
  * to resolve Vercel static export "failed to collect page data" errors.
  */
 import type { MetadataRoute } from 'next'
